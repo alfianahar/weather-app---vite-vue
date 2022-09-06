6ddff7ccca88578f636233c531e149026055e9ca
alfianahar <58506749+alfianahar@users.noreply.github.com>
2022-09-06 23:34:19 +0700

    init project file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,11 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/weather-svgrepo-com.svg" class="logo" alt="Weather Logo" />
-    </a>
+    <RouterView />
   </div>
-  <HelloWorld msg="Weather App" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
+
+<style lang="sass" scoped></style>
