af3b961d8a0faab09342e3fef11cb0eea551cbdc
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-09-12 23:06:56 +0700

    add city card and list

---
+++ src/components/CityList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div v-for="city in savedCities" :key="city.id">
+    <CityCard :city="city" @click="goToCityView(city)" />
+  </div>
+  <p v-if="savedCities.length === 0">
+    No location added. To start tracking a location, search in the field above.
+  </p>
+</template>
+
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import CityCard from "./CityCard.vue";
+
+const savedCities = ref([]);
+const getCities = async () => {
+  if (localStorage.getItem("savedCities")) {
+    savedCities.value = JSON.parse(localStorage.getItem("savedCities"));
+
+    const request = [];
+    savedCities.value.forEach((city) => {
+      request.push(
+        axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${city.coor.lat}&lon=${city.coor.lon}&appid=07936c430539475e355cebe7e54026ad&units=metric`
+        )
+      );
+    });
+
+    const weatherData = await Promise.all(request);
+
+    weatherData.forEach((value, index) => {
+      savedCities.value[index].weather = value.data;
+    });
+  }
+};
+await getCities();
+
+const router = useRouter();
+const goToCityView = (city) => {
+  router.push({
+    name: "cityView",
+    params: { state: city.state, city: city.city },
+    query: { lat: city.coor.lat, lon: city.coor.lon },
+  });
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -36,6 +36,14 @@
         </template>
       </ul>
     </div>
+    <div class="flex flex-col gap-4">
+      <Suspense>
+        <CityList />
+        <template #fallback>
+          <p>Loading...</p>
+        </template>
+      </Suspense>
+    </div>
   </main>
 </template>
 
@@ -44,6 +52,7 @@ import { ref } from "vue";
 import axios from "axios";
 import { useRouter } from "vue-router";
 import API_KEY from "../assets/apikey.js";
+import CityList from "../components/CityList.vue";
 
 const router = useRouter();
 const previewCity = (searchResult) => {
